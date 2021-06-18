c41c491deb3834e9d163c0287fb57ce9850307f7
rifqi23ahmad <rifqi23ahmad@gmail.com>
2021-06-18 14:08:52 +0700

    repository kategori pertanian

--- backend/layer/kategoripertanian/repository.go
+++ backend/layer/kategoripertanian/repository.go
@@ -1,7 +1,6 @@
 package kategoripertanian
 
 import (
-	"Cangkoel-Final-Project/entity"
 	"backend/entity"
 
 	"gorm.io/gorm"
@@ -26,7 +25,7 @@ func NewRepository(db *gorm.DB) *repository {
 
 func (r *repository) FindAll() ([]entity.KategoriPertanian, error) {
 	var k_petani []entity.users
-	if err := r.db.Find(&user).Error; err != nil {
+	if err := r.db.Find(&k_petani).Error; err != nil {
 		return k_petani, err
 	}
 
@@ -34,9 +33,53 @@ func (r *repository) FindAll() ([]entity.KategoriPertanian, error) {
 }
 
 func (r *repository) Create(k_petani entity.KategoriPertanian) (entity.k_petani, error) {
-	var k_petani []entity.KategoriPertanian
+	var k_Petani []entity.KategoriPertanian
 
-	if err := r.db.Find(&k_petani).Error; err != nil {
+	if err := r.db.Find(&k_Petani).Error; err != nil {
+		return k_Petani, err
+	}
+
+	return k_Petani, nil
+}
+
+func (r *repository) FindByID(ID string) (entity.KategoriPertanian, error) {
+
+	var k_petani entity.KategoriPertanian
+
+	err := r.db.Where("id = ?", ID).Find(&k_petani).Error
+	if err != nil {
+		return k_petani, err
+	}
+
+	return k_petani, nil
+}
+
+func (r *repository) DeleteByID(ID string) (string, error) {
+	if err := r.db.Where("id = ?", ID).Delete(&entity.KategoriPertanian{}).Error; err != nil {
+		return "error", err
+	}
+
+	return "success", nil
+}
+
+func (r *repository) UpdateByID(ID string, dataUpdate map[string]interface{}) (entity.KategoriPertanian, error) {
+	var k_petani entity.KategoriPertanian
+
+	if err := r.db.Model(&k_petani).Where("id = ?", ID).Updates(dataUpdate).Error; err != nil {
+		return k_petani, err
+	}
+
+	if err := r.db.Where("id = ?", ID).Find(&k_petani).Error; err != nil {
+		return k_petani, err
+	}
+	return k_petani
+}
+
+func (r *repository) FindByEmail(email string) (entity.KategoriPertanian, error) {
+	var k_petani entity.KategoriPertanian
+
+	err := r.db.Where("email = ?", email).Find(&k_petani).Error
+	if err != nil {
 		return k_petani, err
 	}
 
