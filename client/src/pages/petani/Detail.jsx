cf8014975b16a34a3a425bc7c6a072b976158f54
mohammedazez <muhamadaziz047@gmail.com>
2021-06-18 17:53:17 +0700

    change class to className

--- client/src/pages/petani/Detail.jsx
+++ client/src/pages/petani/Detail.jsx
@@ -23,27 +23,27 @@ const Detail = ({ roundedHeaderButton = false }) => {
     <>
       <Navbar />
       <div className="bg-gray-300">
-        <div class="bg-dark-gray w-full min-h-screen ">
-          <div class="w-full max-w-6xl mx-auto px-4 py-8 flex justify-between md:flex-no-wrap flex-wrap">
-            <div class="md:w-1/3 w-full">
+        <div className="bg-dark-gray w-full min-h-screen ">
+          <div className="w-full max-w-6xl mx-auto px-4 py-8 flex justify-between md:flex-no-wrap flex-wrap">
+            <div className="md:w-1/3 w-full">
               <header>
                 <img
                   src="https://i.ibb.co/fDB3tPT/Group-1.png"
                   alt="Profile"
-                  class="ml-12"
+                  className="ml-12"
                 />
                 <img
                   src="https://i.ibb.co/m8479nW/Web-Developer.png"
                   alt="Logo"
-                  class="mt-4 max-w-sm w-full"
+                  className="mt-4 max-w-sm w-full"
                 />
               </header>
-              <section class="mt-16">
-                <h3 class="uppercase text-white font-medium text-3xl">
+              <section className="mt-16">
+                <h3 className="uppercase text-white font-medium text-3xl">
                   Career Objectives
                 </h3>
-                <div class="h-1 bg-green w-48 my-4"></div>
-                <p class="text-white">
+                <div className="h-1 bg-green w-48 my-4"></div>
+                <p className="text-white">
                   I am a motivated team player and aspiring web developer with
                   great design and branding knowledge. My ultimate goal is to
                   grow my knowledge of the industry and use my conversational
@@ -51,15 +51,18 @@ const Detail = ({ roundedHeaderButton = false }) => {
                 </p>
               </section>
 
-              <section class="mt-16">
-                <h3 class="uppercase text-white font-medium text-3xl">
+              <section className="mt-16">
+                <h3 className="uppercase text-white font-medium text-3xl">
                   Specializations
                 </h3>
-                <div class="h-1 bg-green w-48 my-4"></div>
-                <ul class="text-white list-disc list-inside">
+                <div className="h-1 bg-green w-48 my-4"></div>
+                <ul className="text-white list-disc list-inside">
                   <li>Front End Design (HTML, CSS, Figma, Sass)</li>
                   <li>
-                    <a href="https://tailwindcss.com" class="hover:underline">
+                    <a
+                      href="https://tailwindcss.com"
+                      className="hover:underline"
+                    >
                       TailwindCSS (♥)
                     </a>
                   </li>
@@ -77,67 +80,67 @@ const Detail = ({ roundedHeaderButton = false }) => {
                 </ul>
               </section>
 
-              <section class="mt-16">
-                <h3 class="uppercase text-white font-medium text-3xl">
+              <section className="mt-16">
+                <h3 className="uppercase text-white font-medium text-3xl">
                   Contact Info:
                 </h3>
-                <div class="h-1 bg-green w-48 my-4"></div>
-                <div class="text-white">
+                <div className="h-1 bg-green w-48 my-4"></div>
+                <div className="text-white">
                   <a
                     href="https://linkedin.com/in/justaashir"
-                    class="hover:underline flex items-center"
+                    className="hover:underline flex items-center"
                   >
-                    <ion-icon name="logo-linkedin" class="mr-2"></ion-icon>
+                    <ion-icon name="logo-linkedin" className="mr-2"></ion-icon>
                     LinkedIn
                   </a>
                   <a
                     href="https://twitter.com/justaashir"
-                    class="hover:underline flex items-center mt-1"
+                    className="hover:underline flex items-center mt-1"
                   >
-                    <ion-icon name="logo-twitter" class="mr-2"></ion-icon>{" "}
+                    <ion-icon name="logo-twitter" className="mr-2"></ion-icon>{" "}
                     Twitter
                   </a>
                   <a
                     href="mailto:hello@justaashir.com"
-                    class="hover:underline flex items-center mt-1"
+                    className="hover:underline flex items-center mt-1"
                   >
-                    <ion-icon name="mail" class="mr-2"></ion-icon>{" "}
+                    <ion-icon name="mail" className="mr-2"></ion-icon>{" "}
                     hello@justaashir.com
                   </a>
                   <a
                     href="https://justaashir.com"
-                    class="hover:underline flex items-center mt-1"
+                    className="hover:underline flex items-center mt-1"
                   >
-                    <ion-icon name="globe" class="mr-2"></ion-icon>{" "}
+                    <ion-icon name="globe" className="mr-2"></ion-icon>{" "}
                     www.justaashir.com
                   </a>
                 </div>
               </section>
             </div>
-            <div class="md:w-2/4 w-full">
-              <section class="mt-16 md:mt-0">
-                <h3 class="uppercase text-white font-medium text-3xl">
+            <div className="md:w-2/4 w-full">
+              <section className="mt-16 md:mt-0">
+                <h3 className="uppercase text-white font-medium text-3xl">
                   Work Summary
                 </h3>
 
-                <div class="h-1 bg-green w-48 my-4"></div>
+                <div className="h-1 bg-green w-48 my-4"></div>
                 <PrimaryLink
                   css={roundedHeaderButton && tw`rounded-full`}
                   href="/order-date"
                 >
                   Book Now
                 </PrimaryLink>
-                <div class="mt-8">
-                  <h4 class="font-medium text-green text-2xl">
+                <div className="mt-8">
+                  <h4 className="font-medium text-green text-2xl">
                     Junior Front-end engineer
                   </h4>
-                  <h5 class="text-xl text-green">
-                    <a href="https://renetal.com" class="hover:underline">
+                  <h5 className="text-xl text-green">
+                    <a href="https://renetal.com" className="hover:underline">
                       Renetal
                     </a>{" "}
                     | <i>2019 - JULY 2020</i>
                   </h5>
-                  <ul class="text-white list-disc list-inside mt-4">
+                  <ul className="text-white list-disc list-inside mt-4">
                     <li> Designed high-performant UI Components </li>
                     <li> Complete SaaS app redesign using VueJs </li>
                     <li>
@@ -148,17 +151,20 @@ const Detail = ({ roundedHeaderButton = false }) => {
                   </ul>
                 </div>
 
-                <div class="mt-8">
-                  <h4 class="font-medium text-green text-2xl">
+                <div className="mt-8">
+                  <h4 className="font-medium text-green text-2xl">
                     Founder &lt; CEO
                   </h4>
-                  <h5 class="text-xl text-green">
-                    <a href="https://justifyagency.com" class="hover:underline">
+                  <h5 className="text-xl text-green">
+                    <a
+                      href="https://justifyagency.com"
+                      className="hover:underline"
+                    >
                       Justify Agency
                     </a>{" "}
                     | <i>2020 - Present</i>
                   </h5>
-                  <ul class="text-white list-disc list-inside mt-4">
+                  <ul className="text-white list-disc list-inside mt-4">
                     <li>
                       {" "}
                       Meeting with clients to discuss project requirements and
@@ -173,31 +179,34 @@ const Detail = ({ roundedHeaderButton = false }) => {
                   </ul>
                 </div>
               </section>
-              <section class="mt-16">
-                <h3 class="uppercase text-white font-medium text-3xl">
+              <section className="mt-16">
+                <h3 className="uppercase text-white font-medium text-3xl">
                   Freelance &amp; Other fun stuff
                 </h3>
-                <div class="h-1 bg-green w-48 my-4"></div>
+                <div className="h-1 bg-green w-48 my-4"></div>
 
-                <div class="mt-8">
-                  <h4 class="font-medium text-green text-2xl">
+                <div className="mt-8">
+                  <h4 className="font-medium text-green text-2xl">
                     Shopify Freelance Associate
                   </h4>
-                  <p class="text-white">
+                  <p className="text-white">
                     Proud member of the shopify community, and their partner
                     program. Setting up Shopify stores and making custom themes
                     from a long time.
                   </p>
                 </div>
 
-                <div class="mt-8">
-                  <h4 class="font-medium text-green text-2xl">
-                    <a href="https://dev.to/justaashir" class="hover:underline">
+                <div className="mt-8">
+                  <h4 className="font-medium text-green text-2xl">
+                    <a
+                      href="https://dev.to/justaashir"
+                      className="hover:underline"
+                    >
                       DEV Community
                     </a>{" "}
                     (Volunteer & Technical Writer)
                   </h4>
-                  <ul class="text-white list-disc list-inside mt-4">
+                  <ul className="text-white list-disc list-inside mt-4">
                     <li>
                       Have written about Vuejs, career advice and resources...
                     </li>
@@ -207,34 +216,37 @@ const Detail = ({ roundedHeaderButton = false }) => {
                   </ul>
                 </div>
               </section>
-              <section class="mt-16">
-                <h3 class="uppercase text-white font-medium text-3xl">
+              <section className="mt-16">
+                <h3 className="uppercase text-white font-medium text-3xl">
                   Passion Projects
                 </h3>
-                <div class="h-1 bg-green w-48 my-4"></div>
+                <div className="h-1 bg-green w-48 my-4"></div>
 
-                <div class="mt-8">
-                  <h4 class="font-medium text-green text-2xl">
+                <div className="mt-8">
+                  <h4 className="font-medium text-green text-2xl">
                     <a
                       href="https://tailwindcssuikit.com"
-                      class="hover:underline"
+                      className="hover:underline"
                     >
                       Tailwind CSS Ui Kit
                     </a>
                   </h4>
-                  <p class="text-white mt-2">
+                  <p className="text-white mt-2">
                     Building this, in my free time. Making modern design systems
                     and kits possible with TailwindCSS
                   </p>
                 </div>
 
-                <div class="mt-8">
-                  <h4 class="font-medium text-green text-2xl">
-                    <a href="https://remoteworkjar.com" class="hover:underline">
+                <div className="mt-8">
+                  <h4 className="font-medium text-green text-2xl">
+                    <a
+                      href="https://remoteworkjar.com"
+                      className="hover:underline"
+                    >
                       RemoteWorkJar
                     </a>
                   </h4>
-                  <p class="text-white mt-2">
+                  <p className="text-white mt-2">
                     Remote Job Board, where the main focus is to manually screen
                     every job posted and help candidates get high-quality
                     remote-only job postings.
