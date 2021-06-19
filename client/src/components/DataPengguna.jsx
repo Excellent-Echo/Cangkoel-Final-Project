512a436da82ed5f529b7db043b4e4c1fabbb26cd
mohammedazez <muhamadaziz047@gmail.com>
2021-06-19 11:16:37 +0700

    Done make for page formulir pengajuan

--- client/src/components/DataPengguna.jsx
+++ client/src/components/DataPengguna.jsx
@@ -1,90 +1,86 @@
-import React from "react";
-import styled from "styled-components";
-import tw from "twin.macro";
-import { css } from "styled-components/macro"; //eslint-disable-line
-import {
-  SectionHeading,
-  Subheading as SubheadingBase,
-} from "./misc/Headings.jsx";
-import { SectionDescription } from "./misc/Typography.jsx";
-import { Container, ContentWithPaddingXl } from "./misc/Layouts.jsx";
-import ShieldIconImage from "../assets/shield-icon.svg";
-import CustomizeIconImage from "../assets/customize-icon.svg";
+import React from 'react'
+import styled from 'styled-components'
+import tw from 'twin.macro'
+import { css } from 'styled-components/macro' //eslint-disable-line
+import { SectionHeading, Subheading as SubheadingBase } from './misc/Headings.jsx'
+import { SectionDescription } from './misc/Typography.jsx'
+import { Container, ContentWithPaddingXl } from './misc/Layouts.jsx'
+import ShieldIconImage from '../assets/shield-icon.svg'
+import CustomizeIconImage from '../assets/customize-icon.svg'
 
-const Heading = tw(SectionHeading)``;
-const Subheading = tw(SubheadingBase)`text-center mb-3`;
-const Description = tw(SectionDescription)`text-center mx-auto`;
+const Heading = tw(SectionHeading)``
+const Subheading = tw(SubheadingBase)`text-center mb-3`
+const Description = tw(SectionDescription)`text-center mx-auto`
 const ThreeColumnContainer = styled.div`
-  ${tw`mt-10 flex flex-col items-center lg:items-stretch lg:flex-row flex-wrap lg:justify-center max-w-screen-lg mx-auto`}
-`;
+	${tw`mt-10 flex flex-col items-center lg:items-stretch lg:flex-row flex-wrap lg:justify-center max-w-screen-lg mx-auto`}
+`
 const Column = styled.div`
-  ${tw`lg:w-1/3 max-w-xs`}
-`;
+	${tw`lg:w-1/3 max-w-xs`}
+`
 
 const Card = styled.a`
-  ${tw`flex flex-col items-center text-center h-full mx-4 px-4 py-8 rounded transition-transform duration-300 hover:cursor-pointer transform hover:scale-105 `}
-  .imageContainer {
-    ${tw`text-center rounded-full p-4 bg-gray-100`}
-    img {
-      ${tw`w-8 h-8`}
-    }
-  }
-  .title {
-    ${tw`mt-4 font-bold text-xl leading-8`}
-  }
-  .description {
-    ${tw`mt-4 text-sm font-medium text-secondary-300`}
-  }
-  .link {
-    ${tw`mt-auto inline-flex items-center pt-5 text-sm font-bold text-primary-300 leading-none hocus:text-primary-900 transition duration-300`}
-    .icon {
-      ${tw`ml-2 w-4`}
-    }
-  }
-`;
+	${tw`flex flex-col items-center text-center h-full mx-4 px-4 py-8 rounded transition-transform duration-300 hover:cursor-pointer transform hover:scale-105 `}
+	.imageContainer {
+		${tw`text-center rounded-full p-4 bg-gray-100`}
+		img {
+			${tw`w-8 h-8`}
+		}
+	}
+	.title {
+		${tw`mt-4 font-bold text-xl leading-8`}
+	}
+	.description {
+		${tw`mt-4 text-sm font-medium text-secondary-300`}
+	}
+	.link {
+		${tw`mt-auto inline-flex items-center pt-5 text-sm font-bold text-primary-300 leading-none hocus:text-primary-900 transition duration-300`}
+		.icon {
+			${tw`ml-2 w-4`}
+		}
+	}
+`
 
 const Datapengguna = ({
-  cards = [
-    {
-      imageSrc: ShieldIconImage,
-      title: "Secure",
-      description:
-        "We strictly only deal with vendors that provide top notch security.",
-    },
-    {
-      imageSrc: CustomizeIconImage,
-      title: "Customizable",
-      description: "Lorem ipsum donor amet siti ceali placeholder text",
-    },
-  ],
-  heading = "",
-  subheading = "",
-  description = "",
-  imageContainerCss = null,
-  imageCss = null,
+	cards = [
+		{
+			imageSrc: ShieldIconImage,
+			title: 'Secure',
+			description: 'We strictly only deal with vendors that provide top notch security.'
+		},
+		{
+			imageSrc: CustomizeIconImage,
+			title: 'Customizable',
+			description: 'Lorem ipsum donor amet siti ceali placeholder text'
+		}
+	],
+	heading = '',
+	subheading = '',
+	description = '',
+	imageContainerCss = null,
+	imageCss = null
 }) => {
-  return (
-    <Container>
-      <ContentWithPaddingXl>
-        {subheading && <Subheading>{subheading}</Subheading>}
-        {heading && <Heading>{heading}</Heading>}
-        {description && <Description>{description}</Description>}
-        <ThreeColumnContainer>
-          {cards.map((card, i) => (
-            <Column key={i}>
-              <Card>
-                <span className="imageContainer" css={imageContainerCss}>
-                  <img src={card.imageSrc} alt="" css={imageCss} />
-                </span>
-                <span className="title">{card.title}</span>
-                <p className="description">{card.description}</p>
-              </Card>
-            </Column>
-          ))}
-        </ThreeColumnContainer>
-      </ContentWithPaddingXl>
-    </Container>
-  );
-};
+	return (
+		<Container>
+			<ContentWithPaddingXl>
+				{subheading && <Subheading>{subheading}</Subheading>}
+				{heading && <Heading>{heading}</Heading>}
+				{description && <Description>{description}</Description>}
+				<ThreeColumnContainer>
+					{cards.map((card, i) => (
+						<Column key={i}>
+							<Card>
+								<span className="imageContainer" css={imageContainerCss}>
+									<img src={card.imageSrc} alt="" css={imageCss} />
+								</span>
+								<span className="title">{card.title}</span>
+								<p className="description">{card.description}</p>
+							</Card>
+						</Column>
+					))}
+				</ThreeColumnContainer>
+			</ContentWithPaddingXl>
+		</Container>
+	)
+}
 
-export default Datapengguna;
+export default Datapengguna
