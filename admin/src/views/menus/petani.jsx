a77256e7e7132da46eb1d8d2b549133c31b9e99d
mohammedazez <muhamadaziz047@gmail.com>
2021-06-22 00:00:26 +0700

    add : modal for edit and delete

--- admin/src/views/menus/petani.jsx
+++ admin/src/views/menus/petani.jsx
@@ -1,9 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 import img1 from "../../assets/images/users/1.jpg";
 import img2 from "../../assets/images/users/2.jpg";
-import img3 from "../../assets/images/users/3.jpg";
-import img4 from "../../assets/images/users/4.jpg";
 
 import {
   Card,
@@ -12,9 +10,23 @@ import {
   CardSubtitle,
   Input,
   Table,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
 } from "reactstrap";
 
-const Petani = () => {
+const Petani = (props) => {
+  const { className } = props;
+
+  const [modal, setModal] = useState(false);
+  const [konfirmasi, setKonfirmasi] = useState(false);
+  const toggle = () => setModal(!modal);
+  const show = () => setKonfirmasi(!konfirmasi);
+
   return (
     <Card>
       <CardBody>
@@ -62,6 +74,72 @@ const Petani = () => {
               </td>
               <td>isi data</td>
               <td>isi data</td>
+              <div className="grid-container">
+                <div className="item2">
+                  <Button className="btn" color="primary" onClick={toggle}>
+                    Edit
+                  </Button>
+                </div>
+                <div className="item3">
+                  <Button className="btn" color="secondary" onClick={show}>
+                    Delete
+                  </Button>
+                  <Modal isOpen={modal} toggle={toggle} className={className}>
+                    <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+                    <ModalBody>
+                      <Form>
+                        <FormGroup>
+                          <Label for="exampleEmail">Field</Label>
+                          <Input
+                            type="text"
+                            name="text"
+                            id="name"
+                            placeholder="with a placeholder"
+                          />
+                        </FormGroup>
+                        <FormGroup>
+                          <Label for="examplePassword">Field</Label>
+                          <Input
+                            type="text"
+                            name="text"
+                            id="name"
+                            placeholder="with a placeholder"
+                          />
+                        </FormGroup>
+
+                        <FormGroup>
+                          <Label for="exampleText">Text Area</Label>
+                          <Input type="textarea" name="text" id="exampleText" />
+                        </FormGroup>
+                        <FormGroup>
+                          <Label for="exampleFile">File</Label>
+                          <Input type="file" name="file" id="exampleFile" />
+                        </FormGroup>
+
+                        <Button style={{ margin: "5px" }} onClick={toggle}>
+                          Submit
+                        </Button>
+                        <Button style={{ margin: "5px" }} onClick={toggle}>
+                          Cancel
+                        </Button>
+                      </Form>
+                    </ModalBody>
+                  </Modal>
+
+                  <Modal isOpen={konfirmasi} show={show} className={className}>
+                    <ModalHeader show={show}>Modal title</ModalHeader>
+                    <ModalBody>
+                      <h4>Apakah serius anda ingin mengapus data ini?</h4>
+                      <Button style={{ margin: "5px" }} onClick={show}>
+                        Delete
+                      </Button>
+                      <Button style={{ margin: "5px" }} onClick={show}>
+                        Cancel
+                      </Button>
+                    </ModalBody>
+                  </Modal>
+                </div>
+              </div>
             </tr>
             <tr>
               <td>
@@ -84,46 +162,72 @@ const Petani = () => {
               </td>
               <td>isi data</td>
               <td>isi data</td>
-            </tr>
-            <tr>
-              <td>
-                <div className="d-flex no-block align-items-center">
-                  <div className="mr-2">
-                    <img
-                      src={img3}
-                      alt="user"
-                      className="rounded-circle"
-                      width="45"
-                    />
-                  </div>
-                  <div className="">
-                    <h5 className="mb-0 font-16 font-medium">Julian Josephs</h5>
-                    <span>Josephs@gmail.com</span>
-                  </div>
+              <div className="grid-container">
+                <div className="item2">
+                  <Button className="btn" color="primary" onClick={toggle}>
+                    Edit
+                  </Button>
                 </div>
-              </td>
-              <td>isi data</td>
-              <td>isi data</td>
-            </tr>
-            <tr>
-              <td>
-                <div className="d-flex no-block align-items-center">
-                  <div className="mr-2">
-                    <img
-                      src={img4}
-                      alt="user"
-                      className="rounded-circle"
-                      width="45"
-                    />
-                  </div>
-                  <div className="">
-                    <h5 className="mb-0 font-16 font-medium">Jan Petrovic</h5>
-                    <span>hgover@gmail.com</span>
-                  </div>
+                <div className="item3">
+                  <Button className="btn" color="secondary" onClick={show}>
+                    Delete
+                  </Button>
+                  <Modal isOpen={modal} toggle={toggle} className={className}>
+                    <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+                    <ModalBody>
+                      <Form>
+                        <FormGroup>
+                          <Label for="exampleEmail">Field</Label>
+                          <Input
+                            type="text"
+                            name="text"
+                            id="name"
+                            placeholder="with a placeholder"
+                          />
+                        </FormGroup>
+                        <FormGroup>
+                          <Label for="examplePassword">Field</Label>
+                          <Input
+                            type="text"
+                            name="text"
+                            id="name"
+                            placeholder="with a placeholder"
+                          />
+                        </FormGroup>
+
+                        <FormGroup>
+                          <Label for="exampleText">Text Area</Label>
+                          <Input type="textarea" name="text" id="exampleText" />
+                        </FormGroup>
+                        <FormGroup>
+                          <Label for="exampleFile">File</Label>
+                          <Input type="file" name="file" id="exampleFile" />
+                        </FormGroup>
+
+                        <Button style={{ margin: "5px" }} onClick={toggle}>
+                          Submit
+                        </Button>
+                        <Button style={{ margin: "5px" }} onClick={toggle}>
+                          Cancel
+                        </Button>
+                      </Form>
+                    </ModalBody>
+                  </Modal>
+
+                  <Modal isOpen={konfirmasi} show={show} className={className}>
+                    <ModalHeader show={show}>Modal title</ModalHeader>
+                    <ModalBody>
+                      <h4>Apakah serius anda ingin mengapus data ini?</h4>
+                      <Button style={{ margin: "5px" }} onClick={show}>
+                        Delete
+                      </Button>
+                      <Button style={{ margin: "5px" }} onClick={show}>
+                        Cancel
+                      </Button>
+                    </ModalBody>
+                  </Modal>
                 </div>
-              </td>
-              <td>isi data</td>
-              <td>isi data</td>
+              </div>
             </tr>
           </tbody>
         </Table>
