0683a9e71eab582034c44f9644e1b35dce9ee3c4
rifqi23ahmad <rifqi23ahmad@gmail.com>
2021-06-22 14:07:31 +0700

    update s

--- backend/layer/kategoripertanian/formatter.go
+++ backend/layer/kategoripertanian/formatter.go
@@ -6,11 +6,11 @@ import (
 )
 
 type KPetaniFormat struct {
-	ID           int         `gorm:"PrimaryKey" json:"id"`
-	NamaKategori string      `json:"nama_kategori"`
-	FotoKategori string      `json:"foto_kategori"`
-	Pendanaan    []Pendanaan `gorm:"ForeignKey:KategoriID"`
-	PendanaanID  int         `json:"pendanaan_id"`
+	ID           int                `gorm:"PrimaryKey" json:"id"`
+	NamaKategori string             `json:"nama_kategori"`
+	FotoKategori string             `json:"foto_kategori"`
+	Pendanaan    []entity.Pendanaan `gorm:"ForeignKey:KategoriID"`
+	PendanaanID  int                `json:"pendanaan_id"`
 }
 
 type KPetaniDeleteFormat struct {
