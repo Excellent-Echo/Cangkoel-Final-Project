c8838bfd572798c5b83a7fea8f5222176a5f848b
azezmohammed <muhamadaziz047@gmail.com>
2021-06-24 13:57:52 +0700

    add : success get all kategori pertanian

--- client/src/components/KategoriPertanian.jsx
+++ client/src/components/KategoriPertanian.jsx
@@ -5,6 +5,7 @@ import styled from 'styled-components'
 import { css } from 'styled-components/macro' //eslint-disable-line
 import { useDispatch, useSelector } from 'react-redux'
 import { getKategoriAction } from '../redux/kategori/kategoriAction'
+import { useHistory } from 'react-router-dom'
 
 // components
 import { ContentWithPaddingXl } from './misc/Layouts.jsx'
@@ -29,110 +30,57 @@ const CardButton = tw.span`rounded bg-crowde-100 hover:bg-crowde-200 py-2 px-4 t
 const CardText = tw.div`p-4 text-gray-900`
 const CardTitle = tw.h5`text-lg font-semibold group-hover:text-crowde-100`
 
-const KategoriPertanian = ({
-	tabs = {
-		Starters: [
-			{
-				imageSrc: 'https://res.cloudinary.com/cangkoel/image/upload/v1623941562/sayuran_pgy0to.jpg',
-				title: 'Pendanaan Ekspor Tanaman Sayur',
-				url: '/results'
-			},
-			{
-				imageSrc: 'https://res.cloudinary.com/cangkoel/image/upload/v1623941563/buah_snz3r1.jpg',
-				title: 'Pendanaan Ekspor Tanaman Buah',
-				url: '/results'
-			},
-			{
-				imageSrc: 'https://res.cloudinary.com/cangkoel/image/upload/v1623941562/industri_kwtr3e.jpg',
-				title: 'Pendanaan Ekspor Tanaman Industri',
-				url: '/results'
-			},
-			{
-				imageSrc: 'https://res.cloudinary.com/cangkoel/image/upload/v1623941495/pangan_bgxoqb.jpg',
-				title: 'Pendanaan Ekspor Tanaman Pangan',
-				url: '/results'
-			},
-			{
-				imageSrc: 'https://res.cloudinary.com/cangkoel/image/upload/v1623941562/hias_yp2hle.jpg',
-				title: 'Pendanaan Ekspor Tanaman Hias',
-				url: '/results'
-			},
-			{
-				imageSrc: 'https://res.cloudinary.com/cangkoel/image/upload/v1623941563/umbi_vu2y1i.jpg',
-				title: 'Pendanaan Ekspor Tanaman Umbi',
-				url: '/results'
-			},
-			{
-				imageSrc: 'https://res.cloudinary.com/cangkoel/image/upload/v1623941562/sayuran_pgy0to.jpg',
-				title: 'Pendanaan Ekspor Tanaman Sayur',
-				url: '/results'
-			},
-			{
-				imageSrc: 'https://res.cloudinary.com/cangkoel/image/upload/v1623941562/sayuran_pgy0to.jpg',
-				title: 'Pendanaan Ekspor Tanaman Sayur',
-				url: '/results'
-			}
-		]
-	}
-}) => {
+const KategoriPertanian = () => {
+	const history = useHistory()
 	const dispatch = useDispatch()
 	const listKategori = useSelector((state) => state.kategori)
-	const tabsKeys = Object.keys(tabs)
 	const { kategoriPertanian } = listKategori
-
 	console.log('kategori pertanian', kategoriPertanian)
 
 	useEffect(() => {
 		dispatch(getKategoriAction())
 	}, [dispatch])
 
+	const handleClick = (id) => {
+		history.push(`/results/${id}`)
+	}
+
 	return (
 		<ContentWithPaddingXl>
-			{tabsKeys.map((tabKey, index) => (
-				<TabContent
-					key={index}
-					variants={{
-						current: {
-							opacity: 1,
-							scale: 1,
-							display: 'flex'
-						},
-						hidden: {
-							opacity: 0,
-							scale: 0.8,
-							display: 'none'
-						}
-					}}
-					transition={{ duration: 0.4 }}
-				>
-					{tabs[tabKey].map((card, index) => (
-						<CardContainer key={index}>
-							<Card className="group" href={card.url} initial="rest" whileHover="hover" animate="rest">
-								<CardImageContainer imageSrc={card.imageSrc}>
-									<CardHoverOverlay
-										variants={{
-											hover: {
-												opacity: 1,
-												height: 'auto'
-											},
-											rest: {
-												opacity: 0,
-												height: 0
-											}
-										}}
-										transition={{ duration: 0.3 }}
-									>
-										<CardButton>Cari Pendanaan</CardButton>
-									</CardHoverOverlay>
-								</CardImageContainer>
-								<CardText>
-									<CardTitle>{card.title}</CardTitle>
-								</CardText>
-							</Card>
-						</CardContainer>
-					))}
-				</TabContent>
-			))}
+			<TabContent>
+				{kategoriPertanian.map((item, index) => (
+					<CardContainer key={index}>
+						<Card
+							className="group"
+							onClick={() => handleClick(item.id)}
+							initial="rest"
+							whileHover="hover"
+							animate="rest"
+						>
+							<CardImageContainer imageSrc={item.foto_kategori}>
+								<CardHoverOverlay
+									variants={{
+										hover: {
+											opacity: 1,
+											height: 'auto'
+										},
+										rest: {
+											opacity: 0,
+											height: 0
+										}
+									}}
+									transition={{ duration: 0.3 }}
+								>
+									<CardButton>Cari Pendanaan</CardButton>
+								</CardHoverOverlay>
+							</CardImageContainer>
+							<CardText>
+								<CardTitle>{item.nama_kategori}</CardTitle>
+							</CardText>
+						</Card>
+					</CardContainer>
+				))}
+			</TabContent>
 		</ContentWithPaddingXl>
 	)
 }
