7cb46624437f25c384c6831cb7274e038b5bfba1
azezmohammed <muhamadaziz047@gmail.com>
2021-06-25 21:44:27 +0700

    add : success fetch kategori pertanian

--- admin/src/redux/rootReducers.js
+++ admin/src/redux/rootReducers.js
@@ -1,13 +1,24 @@
 import { combineReducers } from 'redux'
 
+// pengajuan reducers
 import pengajuanReducer from './pengajuan/pengajuanReducer'
+
+// user reducers
 import userLoginReducer from './user/login/userLoginReducer'
 import userProfileReducer from './user/profile/userProfileReducer'
+
+// kategori reducers
+import readKategoriReducers from './kategori/read/readKategoriReducer'
+
+// pendanaan reducers
+import readPendanaanReducers from './pendanaan/read/readPendanaanReducer'
 
 const rootReducers = combineReducers({
 	pengajuan: pengajuanReducer,
 	userLogin: userLoginReducer,
-	userProfile: userProfileReducer
+	userProfile: userProfileReducer,
+	readKategori: readKategoriReducers,
+	readPendanaan: readPendanaanReducers
 })
 
 export default rootReducers
